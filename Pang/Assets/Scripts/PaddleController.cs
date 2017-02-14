cf5528c32c58b759298f870abfc6494c9fff3474
SilentSupernova <chrisyoules93@gmail.com>
2017-02-14 20:26:54 +0000

    Trying new paddle interaction

--- Pang/Assets/Scripts/PaddleController.cs
+++ Pang/Assets/Scripts/PaddleController.cs
@@ -3,14 +3,28 @@
 public class PaddleController : MonoBehaviour
 {
     public Paddle controlledPaddle;
+    public GameObject rayOrigin;
 
     void Update()
     {
-        controlledPaddle.SetPosition((Vector3.ProjectOnPlane(transform.forward * controlledPaddle.PosZ, controlledPaddle.HitDirection) + (controlledPaddle.HitDirection * controlledPaddle.PosZ)));
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin.transform.position, Vector3.forward, out hit))
+        {
+            if(hit.collider.tag == "PaddleWall")
+            controlledPaddle.transform.position = hit.point;
+            //This works but the ray doesn't move with the rotation of the camera? No fuckign clue why probably a shitty VR reason
+        }
+        Debug.DrawRay(rayOrigin.transform.position, Vector3.forward, Color.red);
+
+        //Original Method
+        //controlledPaddle.SetPosition((Vector3.ProjectOnPlane(transform.forward * controlledPaddle.PosZ, controlledPaddle.HitDirection) + (controlledPaddle.HitDirection * controlledPaddle.PosZ)));
 
         if (Input.GetMouseButtonDown(0))
         {
             controlledPaddle.Serve();
         }
+
+
     }
 }
