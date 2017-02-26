25004c15eff400cde110aa5fbcea23bbb5af3d95
SilentSupernova <chrisyoules93@gmail.com>
2017-02-26 21:04:01 +0000

    Music Stuff start needs work

---
+++ Pang/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip gameMusic;
+
+    public AudioSource thisAudioSource;
+    public AudioSource sfxAudio;
+
+    public AudioClip[] sfxAudioClips;
+
+    
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    public void UISelectNoise()
+    {
+        sfxAudio.volume = 0.7f;
+        thisAudioSource.pitch = Random.Range(1f, 1.5f);
+        thisAudioSource.PlayOneShot(sfxAudioClips[1]);
+    }
+
+    public void playGameNoise(int clipNum)
+    {
+        sfxAudio.volume = 0.7f;
+        sfxAudio.PlayOneShot(sfxAudioClips[clipNum]);
+    }
+
+    public void SaveAndExit()
+    {
+        thisAudioSource.volume = PlayerPrefsManager.GetMusicVolume();
+        sfxAudio.volume = PlayerPrefsManager.GetSFXVolume();
+    }
+}
---
+++ Pang/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    public bool musicMuted, sfxMuted;
+
+    const string SFX_VOLUME_KEY = "sfx_volume";
+
+    const string MUSIC_VOLUME_KEY = "music_volume";
+    public MusicManager musicManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (musicMuted)
+        {
+            PlayerPrefs.SetFloat(MUSIC_VOLUME_KEY, 0f);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(MUSIC_VOLUME_KEY, 0.3f);
+        }
+
+        if (sfxMuted)
+        {
+            PlayerPrefs.SetFloat(SFX_VOLUME_KEY, 0f);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(SFX_VOLUME_KEY, 0.7f);
+        }
+    }
+
+    public void MuteMusic()
+    {
+        musicMuted = !musicMuted;
+        musicManager.SaveAndExit();
+    }
+
+    public void MuteSFX()
+    {
+        sfxMuted = !sfxMuted;
+        musicManager.SaveAndExit();
+    }
+
+
+
+    public static float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat(MUSIC_VOLUME_KEY);
+    }
+
+    public static float GetSFXVolume()
+    {
+        return PlayerPrefs.GetFloat(SFX_VOLUME_KEY);
+    }
+}
