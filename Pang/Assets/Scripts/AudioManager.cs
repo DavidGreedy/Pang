7158067ec4c1bdfc4826498c1387c1d28e33100a
SilentSupernova <chrisyoules93@gmail.com>
2017-02-26 17:29:26 +0000

    Sounds yo

---
+++ Pang/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager> {
+
+    public AudioSource menuSource;
+    public AudioClip menuClip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayMenuSound()
+    {
+        menuSource.pitch = Random.Range(1f, 1.75f);
+        menuSource.PlayOneShot(menuClip);
+    }
+
+    public void PlaySoundOnce(AudioSource source, AudioClip clip)
+    {
+        source.PlayOneShot(clip);
+    }
+}
