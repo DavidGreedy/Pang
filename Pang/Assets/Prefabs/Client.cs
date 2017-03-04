05a9ad2c718c6659a1f17a2888498c23e9b00d51
SilentSupernova <chrisyoules93@gmail.com>
2017-03-04 19:18:15 +0000

    Google Play Push

--- Pang/Assets/Prefabs/Client.cs
+++ Pang/Assets/Prefabs/Client.cs
@@ -6,6 +6,8 @@ using UnityEngine.Networking;
 public class Client : MonoBehaviour
 {
     NetworkManager networkManager;
+    public bool twoPlayers;
+    public int playerCount;
 
     void Start()
     {
@@ -17,4 +19,29 @@ public class Client : MonoBehaviour
     {
         networkManager.StartClient();
     }
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        playerCount++;
+        CheckPlayerCount();
+    }
+
+    void OnPlayerDisonnected(NetworkPlayer player)
+    {
+        playerCount++;
+        CheckPlayerCount();
+    }
+
+    void CheckPlayerCount()
+    {
+        if (playerCount == 2)
+        {
+            twoPlayers = true;
+        }
+        else if (playerCount < 2)
+        {
+            twoPlayers = false;
+        }
+    }
+
 }
---
+++ Pang/Assets/Scripts/GooglePlay/GPG.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+public class GPG : MonoBehaviour
+{
+
+    public string leaderboard;
+
+
+
+    void Start()
+    {
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+        LogIn();
+
+        
+    }
+
+    public void LogIn()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                Debug.Log("Login Sucess");
+            }
+            else
+            {
+                Debug.Log("Login failed");
+            }
+        });
+    }
+
+
+    public void OnShowLeaderBoard()
+    {
+        //		Social.ShowLeaderboardUI (); // Show all leaderboard
+        ((PlayGamesPlatform)Social.Active).ShowLeaderboardUI(leaderboard); // Show current (Active) leaderboard
+        Debug.Log(leaderboard);
+    }
+
+    public void OnAddScoreToLeaderBoard()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportScore(100, leaderboard, (bool success) =>
+            {
+                if (success)
+                {
+                    Debug.Log("Update Score Success");
+
+                }
+                else
+                {
+                    Debug.Log("Update Score Fail");
+                }
+            });
+        }
+    }
+
+    public void OnLogOut()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+    }
+
+}
