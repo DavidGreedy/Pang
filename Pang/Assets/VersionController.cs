34e44a64780a81ea24532f57db8563813bd67d68
SilentSupernova <chrisyoules93@gmail.com>
2017-03-10 22:03:19 +0000

    Added in a version thingy

---
+++ Pang/Assets/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionController : Singleton<VersionController> {
+    
+    public enum VersionType {Paid, Free}
+
+    [SerializeField]
+    private VersionType versionType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
